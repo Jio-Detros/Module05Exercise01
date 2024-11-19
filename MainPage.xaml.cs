39cebd324a338530d3abf037c85f7e19c4771003
Jio <detros.jiorhey@auf.edu.ph>
2024-11-19 12:24:29 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Module05Exercise01.Service;
+using MySql.Data.MySqlClient;
+
+namespace Module05Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private readonly DatabaseConnectionService _databaseConnectionService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _databaseConnectionService = new DatabaseConnectionService();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private async void OnTestConnectionClicked(object sender, EventArgs e)
+        {
+            var connectionString = _databaseConnectionService.GetConnectionString();
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    ConnectionStatusLabel.Text = "Connection Successful";
+                    ConnectionStatusLabel.TextColor = Colors.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                ConnectionStatusLabel.Text = $"Connection Failed: {ex.Message}";
+                ConnectionStatusLabel.TextColor = Colors.Red;
+            }
+        }
+
+        private async void OpenViewEmployee(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ViewPersonal");
+        }
+    }
+}
